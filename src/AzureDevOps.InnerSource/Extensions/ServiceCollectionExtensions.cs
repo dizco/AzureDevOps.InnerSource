2b7f090441845f4e1ec0471408cbcdbcf7f27ee7
Gabriel <dizco@users.noreply.github.com>
2023-07-29 13:36:04 -0400

    Add

--- src/AzureDevOps.InnerSource/Extensions/ServiceCollectionExtensions.cs
+++ src/AzureDevOps.InnerSource/Extensions/ServiceCollectionExtensions.cs
@@ -6,11 +6,9 @@ using AzureDevOps.InnerSource.Configuration;
 using AzureDevOps.InnerSource.Configuration.Settings;
 using AzureDevOps.InnerSource.Services;
 using AzureDevOps.InnerSource.Storage;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.Extensions.Options;
-using Microsoft.Extensions.Primitives;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.VisualStudio.Services.Common;
 using Microsoft.VisualStudio.Services.WebApi;
@@ -105,14 +103,9 @@ public static class ServiceCollectionExtensions
 				// Allows the Azure DevOps extension tokens
 				// See: https://learn.microsoft.com/en-us/azure/devops/extend/develop/auth?view=azure-devops#net-framework
 
-				//var secret =
-				//	"eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Im9PdmN6NU1fN3AtSGpJS2xGWHo5M3VfVjBabyJ9.eyJjaWQiOiIwMzZjMjViNy02MzlmLTQyOGEtOTlkYy0zMzUwYWJjNTE1ZGEiLCJjc2kiOiJhZDU1OTc2YS05OTgyLTRiNTMtYWMzMS01YjVmMjA4NjQ0YjUiLCJuYW1laWQiOiIwMDAwMDAyOS0wMDAwLTg4ODgtODAwMC0wMDAwMDAwMDAwMDAiLCJpc3MiOiJhcHAudnN0b2tlbi52aXN1YWxzdHVkaW8uY29tIiwiYXVkIjoiYXBwLnZzdG9rZW4udmlzdWFsc3R1ZGlvLmNvbSIsIm5iZiI6MTY4ODI0OTU3MSwiZXhwIjoxODQ2MTAyMzcxfQ.HYFSVshJhozP9t1fhDniSgN-AJT2dSuoeutJPex907xxZTZ1zK8sJq9rPOkZmMM1Q-3d0xoYorlw1rHKTE7LYJAAFhf7q2YvgWnFYpYovwNWFnxyquCWR5E_dEyLMxpq2RkjjOGxMRZbgXpOGxCBhUUp00-dBU4bYRVA7moIAx-iiK8xrh8BMgtjDj1czKPhRTnIrPsAGmu4GE6cV7acXoaSlM0V3bTHZRn_IllnwqOv3dEUFVsA3O0SWjCBwRR0nYPrYOitRj5GN5VMfIOPm8WfbZDzXSKU72PHMO2cA_O8CrlBk9HRcgBPtB1wYl6Tm4eq2Ps8DGMtmg9GD2g0xw";
-				var secret =
-					"eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Im9PdmN6NU1fN3AtSGpJS2xGWHo5M3VfVjBabyJ9.eyJjaWQiOiJjNWQzNmM0OC1kMTE4LTQ5ODItOTRlMS1kNzc2OTczNjYxOTEiLCJjc2kiOiJjYzBlODNjNC0yODZmLTRhMjUtODc5Ni05NmE2NGQwNmUzNDYiLCJuYW1laWQiOiIwMDAwMDAyOS0wMDAwLTg4ODgtODAwMC0wMDAwMDAwMDAwMDAiLCJpc3MiOiJhcHAudnN0b2tlbi52aXN1YWxzdHVkaW8uY29tIiwiYXVkIjoiYXBwLnZzdG9rZW4udmlzdWFsc3R1ZGlvLmNvbSIsIm5iZiI6MTY4ODkzNTUyMiwiZXhwIjoxODQ2Nzg4MzIyfQ.MlLAEE3yDsX0CDNtV0L0pXJS909Vd5PcHVuFXFShUMD9UAfiaZnAloKULeqahKAdvnx2bCc_wKzvkqEYIDPQy_vBgm87ANrgFhuKYkx2zWn-EwedMIl3oHDrQ4QKhL9nEjznP4vC8BSHHuIsG4-YUJtX0JPEjCVzWkUyJpw3JpD-33H3tjpBVg76dZyBdO4jCne-jc7JSLneYn4LW4lfhsL3j-7r9Lq6X7Puj7LypUSKPQHGV0Pt5jy95mQmG-p1DyLE9c4WzCpYOmueYZWTtaon32Mq-mAYSM-_AVHgR7XjU42r_frf5MFREAYIxbWoohrQFPIrwyWo-XNr064t-g";
-
 				options.TokenValidationParameters = new TokenValidationParameters
 				{
-					IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret)),
+					IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authenticationSettings.AzureDevOpsKey)),
 					ValidateIssuer = false,
 					RequireSignedTokens = true,
 					RequireExpirationTime = true,
@@ -134,35 +127,6 @@ public static class ServiceCollectionExtensions
 					ValidateLifetime = true,
 					ValidateAudience = true,
 					ValidAudience = authenticationSettings.Audience
-				};
-
-				options.Events = new JwtBearerEvents
-				{
-					OnAuthenticationFailed = ctx =>
-					{
-						var t = 0;
-						return Task.CompletedTask;
-					},
-					OnChallenge = ctx =>
-					{
-						var t = 1;
-						return Task.CompletedTask;
-					},
-					OnForbidden = ctx =>
-					{
-						var t = 2;
-						return Task.CompletedTask;
-					},
-					OnMessageReceived = ctx =>
-					{
-						var t = 3;
-						return Task.CompletedTask;
-					},
-					OnTokenValidated = ctx =>
-					{
-						var t = 4;
-						return Task.CompletedTask;
-					}
 				};
 			})
 			.AddJwtBearer("AzureDevOpsBadge", options =>
@@ -217,10 +181,6 @@ public static class ServiceCollectionExtensions
 			options.DefaultPolicy = new AuthorizationPolicyBuilder()
 				.RequireAuthenticatedUser()
 				.Build();
-
-			// TODO: Define authorization policies
-			/*options.AddPolicy("BadgeReader", builder => builder.RequireAuthenticatedUser()
-				.RequireClaim("scope", "badges.read"));*/
 		});
 
 		return services;
