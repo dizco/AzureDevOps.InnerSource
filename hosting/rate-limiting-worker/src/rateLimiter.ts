f98447e3b041abf57ad2fa4824c496c5f2242b26
Gabriel <dizco@users.noreply.github.com>
2023-08-30 08:55:41 -0400

    Update rate limiting limits (#18)

--- hosting/rate-limiting-worker/src/rateLimiter.ts
+++ hosting/rate-limiting-worker/src/rateLimiter.ts
@@ -2,39 +2,58 @@ import { MultiRegionRatelimit } from '@upstash/ratelimit';
 import { Redis } from '@upstash/redis/cloudflare';
 
 export interface RateLimitResult {
-		success: boolean;
-		retryAfterSeconds: number;
+  success: boolean;
+  retryAfterSeconds: number;
 }
 
 export class RateLimiterOptions {
-		public constructor(
-				public readonly redisUrl: string,
-				public readonly redisToken: string
-		) {}
+  public constructor(
+    public readonly redisUrl: string,
+    public readonly redisToken: string
+  ) {}
 }
 
 export class RateLimiter {
-		public constructor(private readonly cache: Map<any, any>, private readonly options: RateLimiterOptions) {}
+  private readonly ratelimit: {api: MultiRegionRatelimit, assets: MultiRegionRatelimit};
 
-		public async apply(request: Request): Promise<RateLimitResult> {
-				const ratelimit = new MultiRegionRatelimit({
-						redis: [
-								new Redis({
-										url: this.options.redisUrl,
-										token: this.options.redisToken,
-								}),
-						],
-						limiter: MultiRegionRatelimit.fixedWindow(10, "60 s"),
-						ephemeralCache: this.cache,
-				});
+  public constructor(cache: Map<any, any>, options: RateLimiterOptions) {
+    const redis = new Redis({
+      url: options.redisUrl,
+      token: options.redisToken,
+    });
 
-				const userIP: string = request.headers.get("CF-Connecting-IP") || "none";
+    this.ratelimit = {
+      api: new MultiRegionRatelimit({
+        redis: [redis],
+        limiter: MultiRegionRatelimit.slidingWindow(20, "60 s"),
+        prefix: "ratelimit:api",
+        ephemeralCache: cache,
+      }),
+      assets: new MultiRegionRatelimit({
+        redis: [redis],
+        limiter: MultiRegionRatelimit.slidingWindow(200, "60 s"),
+        prefix: "ratelimit:assets",
+        ephemeralCache: cache,
+      })
+    };
+  }
 
-				const response = await ratelimit.limit(userIP);
+  public async apply(request: Request): Promise<RateLimitResult> {
+    const rateLimiter = this.selectRateLimiter(request);
+    const userIP: string = request.headers.get("CF-Connecting-IP") || "none";
+    const response = await rateLimiter.limit(userIP);
 
-				return {
-						...response,
-						retryAfterSeconds: Math.ceil((response.reset - new Date().getTime()) / 1000),
-				}
-		}
+    return {
+      ...response,
+      retryAfterSeconds: Math.ceil((response.reset - new Date().getTime()) / 1000),
+    }
+  }
+
+  private selectRateLimiter(request: Request): MultiRegionRatelimit {
+    if (request.method === "POST" || request.method === "DELETE" || request.method === "PUT" || request.method === "PATCH"
+      || (request.method !== "OPTIONS" && new RegExp("^/([^/]*)/repositories").test(new URL(request.url).pathname))) {
+      return this.ratelimit.api;
+    }
+    return this.ratelimit.assets;
+  }
 }
