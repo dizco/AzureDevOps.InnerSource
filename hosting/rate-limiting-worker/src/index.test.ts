95c90a39c78a5d1decfd762fa538aa62c98197f5
Gabriel <dizco@users.noreply.github.com>
2023-12-25 17:14:55 -0500

    Fix test

--- hosting/rate-limiting-worker/src/index.test.ts
+++ hosting/rate-limiting-worker/src/index.test.ts
@@ -14,7 +14,8 @@ describe('Worker', () => {
   });
 
   it('should return 200 response', async () => {
-    const resp = await worker.fetch();
+    const req = new Request('https://example.com', {method: 'GET'});
+    const resp = await worker.fetch(req.url);
     expect(resp.status).toBe(200);
   });
 });
